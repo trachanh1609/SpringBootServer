88e7c3b63d068c5452770eea0564465e8a85a1ed
trachanh1609 <Vu@Users-MBP.home>
2017-09-15 08:43:42 +0300

    iinit

---
+++ hellothymeleaf/src/main/java/haagahelia/course/hellothymeleaf/HellothymeleafApplication.java
@@ -0,0 +1,12 @@
+package haagahelia.course.hellothymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellothymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellothymeleafApplication.class, args);
+	}
+}
---
+++ hellothymeleaf/src/test/java/haagahelia/course/hellothymeleaf/HellothymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package haagahelia.course.hellothymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellothymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
